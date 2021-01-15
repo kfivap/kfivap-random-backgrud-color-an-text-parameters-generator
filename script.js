3e4b7b39ed9d257f7bece14f550948f9408f59d7
kfivap <77047958+kfivap@users.noreply.github.com>
2021-01-15 18:28:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+let winheight = window.innerHeight
+let winwidth =window.innerWidth
+let ofheight=document.getElementById('hello').offsetHeight
+let ofwidth=document.getElementById('hello').offsetWidth
+
+
+
+///////////////
+
+
+
+
+var timing1 = 500,
+    textspeed = document.getElementById('textspeed');
+function loop1() {
+document.getElementById('hello').style.fontSize=(`${getRandomArbitrary(40,winwidth*0.4)}px`)
+document.getElementById('hello').style.fontSize=(`${getRandomArbitrary(20,winwidth*0.1)}px`)
+document.getElementById('hello').style.marginTop = (`${getRandomInt(winheight*0.7-ofheight)}px`);
+document.getElementById('hello').style.marginLeft = (`${getRandomInt(winwidth*1.7-ofwidth)}px`);
+document.getElementById('hello').style.color = (`rgb(${getRandomInt(255)},
+		${getRandomInt(255)},
+		${getRandomInt(255)})`);
+
+  window.setTimeout(loop1, timing1);
+}
+document.getElementById('textspeed').addEventListener('change', function (e) {
+  timing1 = parseInt(this.value);
+});
+
+
+var timing2 = 500,
+    textspeed = document.getElementById('colorspeed');
+function loop2() {
+	document.body.style.background=(`rgb(${getRandomInt(255)},
+		${getRandomInt(255)},
+		${getRandomInt(255)})`)
+
+
+  window.setTimeout(loop2, timing2);
+}
+document.getElementById('colorspeed').addEventListener('change', function (e) {
+  timing2 = parseInt(this.value);
+});
+
+
+
+loop1()
+loop2()
